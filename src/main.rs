050e0780061202abefc19a5ee05b424b23df423a
StorkSquatch <60884387+StorkSquatch@users.noreply.github.com>
2024-01-07 20:31:58 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@
 #![no_main]
 use core::panic::PanicInfo;
 
-/// This function is called on panic.
+//called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-/// On Start
+//on start
 #[no_mangle] 
 pub extern "C" fn _start() -> ! {
     vga_buffer::print_s();
